5ca5be1cc8d33aa4dace361e2733c94d0738df3a
Nehal Bobhate <nehalwork05@gmail.com>
2024-08-07 06:29:19 +0530

    First Commit-Started a new Project called Tic-Tac-Toe while learning React.  In this commit I have added headline and created one component called as player.

---
+++ src/Components/Player.jsx
@@ -0,0 +1,12 @@
+export function Player({ name, symbol }) {
+  function clickHandle() {}
+  return (
+    <li>
+      <span className="player">
+        <span className="player-name">{name}</span>
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button>Edit</button>
+    </li>
+  );
+}
